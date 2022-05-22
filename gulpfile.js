32ccd75fbab9905d649d3579aa87d863eec815d4
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-22 14:46:40 -0400

    update gulpfile and maincss w/ postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const del = require('del')
+const postcss = require('gulp-postcss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
 
 gulp.task('styles', () => {
+	const plugins = [autoprefixer(), cssnano()]
+
 	return gulp
 		.src('styles/main.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(plugins))
 		.pipe(gulp.dest('./'))
 })
 
